29203fbfe94402412ba4b99767ae4f353c1b094c
Himanshu Shukla <60007820+himanshu7321@users.noreply.github.com>
2026-06-28 18:28:39 +0530

    Update gemini.js
    key updated

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -36,7 +36,7 @@ exports.handler = async function(event) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite-preview-06-17:generateContent?key=${GEMINI_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_KEY}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
